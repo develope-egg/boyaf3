9770f958107cdd06fc38311707e7063914f2a072
develope-egg <61379940+develope-egg@users.noreply.github.com>
2022-07-03 21:01:13 +0300

    Update tests.py

--- boyaf3/myapp/tests.py
+++ boyaf3/myapp/tests.py
@@ -2,12 +2,12 @@ from django.test import TestCase
 
 # Create your tests here.
 from .views import index, login , png ,merge, word , tools, logout_view , blog ,blogs 
-index(request)
-login(request) 
-png(request)
-merge(request) 
-word(request)  
-tools(request)
-logout_view(request) 
-blog(request)  
-blogs(request)  
+index(self)
+login(self) 
+png(self)
+merge(self) 
+word(self)  
+tools(self)
+logout_view(self) 
+blog(self)  
+blogs(self)  
